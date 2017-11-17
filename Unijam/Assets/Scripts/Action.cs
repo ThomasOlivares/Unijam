54be99f7f30abaf7ed458355b22458cd68ea7a36
Witegys <alexandre.salman@telecom-sudparis.eu>
2017-11-17 23:45:14 +0100

    Actions v 1.0

---
+++ Unijam/Assets/Scripts/Action.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action : MonoBehaviour {
+
+    public enum ActionType
+    {
+        Cut,
+        Destroy,
+        Shoot,
+        Move,
+        Freeze
+    };
+
+    [SerializeField] protected float actionRadius;
+    private ActionType type;
+
+    void Activate()
+    {
+
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Unijam/Assets/Scripts/Cut.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cut : Action {
+
+	// Use this for initialization
+	void Start () {
+        type = ActionType.Cut;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Activate(Vector3 positionPlayer)
+    {
+        foreach (Collider collider in Physics.OverlapSphere(positionPlayer, actionRadius))
+        {
+            obstacle = collider.gameObject.GetComponent<Obstacle>();
+            if (obstacle)
+            {
+                if (obstacle.type == ObstacleType ;
+            }
+        }
+    }
+}
