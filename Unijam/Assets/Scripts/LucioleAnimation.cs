0928dd8b0fd09e429fc35c8dc4473094f85e666d
Kagigz <k.giguz@hotmail.fr>
2017-11-18 11:12:50 +0100

    Luciole Animation

---
+++ Unijam/Assets/Scripts/LucioleAnimation.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LucioleAnimation : MonoBehaviour {
+
+    Transform playerTransform;
+    bool isPlayerMoving;
+
+    float offsetX;
+
+    Vector3 m_centerPosition;
+    float m_degrees;
+
+    float m_speed;
+    float m_amplitude;
+
+    [SerializeField]
+    float m_speed_idle = 1.0f;
+    [SerializeField]
+    float m_speed_move = 1.0f;
+
+    [SerializeField]
+    float m_amplitude_idle = 0.05f;
+    [SerializeField]
+    float m_amplitude_move = 0.2f;
+
+    [SerializeField]
+    float m_period = 1.0f;
+
+    void Start()
+    {
+        offsetX = transform.position.x - transform.parent.position.x;
+    }
+
+    void Update()
+    {
+
+        playerTransform = transform.parent;
+        isPlayerMoving = GetComponentInParent<Character>().isMoving;
+        if (isPlayerMoving)
+        {
+            m_amplitude = m_amplitude_move;
+            m_speed = m_speed_move;
+        }
+        else
+        {
+            m_amplitude = m_amplitude_idle;
+            m_speed = m_speed_idle;
+        }
+        m_centerPosition = playerTransform.position;
+
+        float deltaTime = Time.deltaTime;
+
+        // Move center along x axis
+        m_centerPosition.x += deltaTime * m_speed + offsetX;
+
+        // Update degrees
+        float degreesPerSecond = 360.0f / m_period;
+        m_degrees = Mathf.Repeat(m_degrees + (deltaTime * degreesPerSecond), 360.0f);
+        float radians = m_degrees * Mathf.Deg2Rad;
+
+        // Offset by sin wave
+        Vector3 offset = new Vector3(0.0f, m_amplitude * Mathf.Sin(radians), 0.0f);
+        transform.position = m_centerPosition + offset;
+    }
+}
